3d3e5c11805d9ec939434c17ef1a04979f69bda4
Brandon Wolfe <blwolfe@andrew.cmu.edu>
2014-02-07 20:02:47 -0500

    Initial bugmenot read. Next is to add BeautifulSoup to parse the resulting page.

--- bugscraper.py
+++ bugscraper.py
@@ -1,4 +1,12 @@
 from bs4 import BeautifulSoup
+import urllib2
+bmn_req = urllib2.Request('http://bugmenot.com/', headers={'User-agent' : 'Mozilla/5.0 (Windows NT 6.1; WOW64)'})
+try:
+	response = urllib2.urlopen(bmn_req)
+except urllib2.HTTPError, e:
+	print "Error code: ", e.code
+	print e.fp.read()
+	raise e
 
-print "Hello World!"
-print "Do it big!"
+html = response.read()
+print html

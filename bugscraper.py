b355c66cf0bbe71910640be3d61e9ac22c1b74f9
Brandon Wolfe <blwolfe@andrew.cmu.edu>
2014-02-23 17:04:49 -0500

    Cleaned up a lot of stuff that was returning lists instead of strings. Changed the writout function to write out a CSV. Added parsing for more stats (votes and age). Fixed a bug where the program would crash if certain arguments weren't passed.

--- bugscraper.py
+++ bugscraper.py
@@ -42,6 +42,8 @@ def scrape(url):
 		usernames = []
 		passwords = []
 		rates = []
+		votes = []
+		ages = []
 
 		for account in bmn_soup:
 			# First, parse the accounts for usernames and passwords
@@ -49,19 +51,24 @@ def scrape(url):
 			for userpass in BeautifulSoup(str(account)).findAll('kbd'):
 				if counter == 0: # we have a username
 					counter += 1
-					usernames.append(userpass.contents)
+					usernames.append(userpass.string)
 				elif counter == 1: # we have a password
 					counter += 1
-					passwords.append(userpass.contents)
+					passwords.append(userpass.string)
 				else: # we have a comment, ignore it and reset counter
 					counter = 0
-				# Next, parse for the success rates
-				for success in BeautifulSoup(str(account)).findAll(class_='success_rate'):
-					rates.append(success.contents)
+			# Next, parse for the various stats
+			for item in BeautifulSoup(str(account)).findAll(class_='stats')[1].children:
+				if str(type(item)).find("element.Tag") >= 0:
+					stats_soup = BeautifulSoup(str(item)).findAll('li')
+					# index 0 = success rate, 1 = votes, 2 = age
+					rates.append(stats_soup[0].string)
+					votes.append(stats_soup[1].string)
+					ages.append(stats_soup[2].string)
 
 		# return the list of tuples for later parsing
 		bugmenot_response.close()
-		return zip(usernames, passwords, rates)
+		return zip(usernames, passwords, rates, votes, ages)
 	except urllib2.HTTPError, e:
 		print "Error code: ", e.code
 		# in the odd case of 404, keep going
@@ -77,20 +84,20 @@ Write out the results to a file
 TODO: Add more stats (numpy?)
 """
 def write_result(url, results, log):
+	# result tuple = (user, pass, success %, votes, age)
 	with open(log, 'a+') as logfile:
 		stats = [] # for some averages and whatnot
-		logfile.write(url + '\n')
-		logfile.write("Results: " + str(len(results)) + '\n')
 		for r in results:
-			stats.append(int((str(r[2][0]).rsplit('%'))[0]))
-		logfile.write("Average success %: " + str(sum(stats)/len(stats)) + '\n')
+			logfile.write(url + ',')
+			logfile.write(','.join([d if d is not None else "#None#" for d in r]))
+			logfile.write('\n')
 		logfile.flush()
 		fsync(logfile)
 
 def parse_args():
 	parser = argparse.ArgumentParser(description='Scrape BugMeNot for valid accounts')
 	parser.add_argument('-f', '--file', nargs=1, help='Site list for scraping',
-		default='alexa_top_1m.csv')
+		default=['alexa_top_1m.csv'])
 	parser.add_argument('-g', '--generate', nargs=1,
 		help="""Use the Alexa list instead and write out working sites
 		to a new file.""")
@@ -99,7 +106,7 @@ def parse_args():
 	parser.add_argument('-m', '--max-sites', nargs=1, help="Max sites to parse",
 		default=[1000000])
 	parser.add_argument('-o', '--output', nargs=1,
-		default='result_' + strftime("%m-%d-%Y_%H:%M:%S", localtime()) + '.txt',
+		default=['result_' + strftime("%m-%d-%Y_%H:%M:%S", localtime()) + '.txt'],
 		help='Result output file. Defaults to current date and time.')
 	parser.add_argument('-s', '--skip', nargs=1, default=[1], 
 		help='Skip to entry X before scraping')
@@ -127,28 +134,29 @@ def main(scrape_file, gen_file, min_wait=1.0, max_wait=3.5, **kwargs):
 
 	try:
 		with open(scrape_file, 'r') as to_scrape:
-			with open(gen_file[0], 'w+') as gen_file:
-				site_counter = kwargs['site_counter'] # loop break, default=1
-				result_number = 1 # counter for filtered set
-				for site in to_scrape:
-					url = site.rsplit(',')[1].strip()
-					url_num = site.rsplit(',')[0].strip()
-					# --skip option takes effect here
-					if int(url_num) == int(site_counter):
-						# get the result, None = failure
-						site_result = scrape(url)
-						if site_result:
-							# record the results
-							report_results(url, site_result, gen_file,
-								result_number, kwargs['writeout'],
-								kwargs['logfile'])
-							result_number += 1
-						if site_counter >= int(kwargs['site_counter']) +\
-						 int(kwargs['max_sites'] - 1):
-							break
-						# don't want to DoS...
-						sleep(random.uniform(min_wait, max_wait))
-						site_counter += 1
+			if (gen_file):
+				genfile = open(gen_file[0], 'w+')
+			site_counter = kwargs['site_counter'] # loop break, default=1
+			result_number = 1 # counter for filtered set
+			for site in to_scrape:
+				url = site.rsplit(',')[1].strip()
+				url_num = site.rsplit(',')[0].strip()
+				# --skip option takes effect here
+				if int(url_num) == int(site_counter):
+					# get the result, None = failure
+					site_result = scrape(url)
+					if site_result:
+						# record the results
+						report_results(url, site_result, gen_file,
+							result_number, kwargs['writeout'],
+							kwargs['logfile'])
+						result_number += 1
+					if site_counter >= int(kwargs['site_counter']) +\
+					 int(kwargs['max_sites'] - 1):
+						break
+					# don't want to DoS...
+					sleep(random.uniform(min_wait, max_wait))
+					site_counter += 1
 			
 	except IOError, e:
 		raise IOError("File " + e.filename + " does not exist!")
@@ -165,5 +173,5 @@ def main(scrape_file, gen_file, min_wait=1.0, max_wait=3.5, **kwargs):
 if __name__ == "__main__":	
 	args = parse_args()
 	main(writeout=args.no_results, max_sites=int(args.max_sites[0]),
-		logfile=args.output, scrape_file=args.file,
+		logfile=args.output[0], scrape_file=args.file[0],
 		gen_file=args.generate, site_counter=int(args.skip[0]))

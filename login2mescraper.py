8627af28588d64f6d4e61eafd4260242e2b6e0bc
Gradous <blwolfe@andrew.cmu.edu>
2014-03-13 09:25:21 -0400

    Looks all good now, full browser emulation scraping working.
    Possible TODOs:
    - Tweak wait parameters for quicker tests without triggering captcha
    - Set the user agent to something other than the Selenium one

--- login2mescraper.py
+++ login2mescraper.py
@@ -1,69 +1,62 @@
-from bs4 import BeautifulSoup
 from time import localtime, strftime, sleep
 from os import remove, fsync
+from selenium import webdriver
+from selenium.common.exceptions import NoSuchElementException, WebDriverException
 import argparse
 import random
 import sys
-import urllib2
 
 
-"""
-Globals
-"""
-USER_AGENT = 'FakeAccount-Scraper (github.com/Gradous/FakeAccount-Scraper)'
+# http://selenium-release.storage.googleapis.com/index.html?path=2.40/
 
 """
-Main scraping/spidering function
+Main scraping/spidering function with browser emulation
 """
-
-def scrape(url):
-	# A custom user agent!
-	fakeaccount_req = urllib2.Request('http://fakeaccount.net/login/' + url,
-		headers={'User-agent' : USER_AGENT})
+def scrape(url, min_wait, max_wait):
+	# browser driver setup
+	# since we're using the Remote webdriver, there is no need to
+	# close this later (since it's a separate program)
+	browser = webdriver.Remote("http://localhost:4444/wd/hub", \
+	webdriver.DesiredCapabilities.HTMLUNITWITHJS)
+	# need some waiting due to page load time
+	browser.implicitly_wait(5)
 	try:
-		fakeaccount_response = urllib2.urlopen(fakeaccount_req)
-		# Extract the entries for accounts from the BeautifulSoup
-		fa_soup = BeautifulSoup(fakeaccount_response.read()).find("div", {"id" : "items"})
-		fa_soup = BeautifulSoup(str(fa_soup)).findAll("div", {"class" : "item"})
+		browser.get('http://login2.me#' + url)
+		#print browser.page_source
 
-		""" 
-		The soup will be empty if the page has no accounts or falls into the "bad"
-		category (paywalled, commmunity, etc.)
-		"""
-		if not fa_soup:
-			print "No results for", url, "!"
-			fakeaccount_response.close()
-			return None
+		# OPTION - get the search box and search instead of direct link
+		#search = browser.find_element_by_id("url_input")
+		#search.send_keys(url)
+		# get the submit element and click
+		#browser.find_element_by_id("search_form").submit()
 
 		# Buckets for parsing
 		usernames = []
 		passwords = []
-		rates = []
-		votes = []
-		for account in fa_soup:
-			# this was significantly easier than BugMeNot
-			usernames.append(BeautifulSoup(str(account)).\
-			find("span", {"class" : "login"}).text)
-			passwords.append(BeautifulSoup(str(account)).\
-			find("span", {"class" : "password"}).text)
-			rates.append(BeautifulSoup(str(account)).\
-			find("span", {"class" : "rating"}).text.strip())
-			# split off a little extra text from the votes
-			votes.append(BeautifulSoup(str(account)).\
-			find("span", {"class" : "votes"}).text[1:-1])
-
+		try:
+			# Keep looping until there are no more accounts to show
+			more_button_xpath = '//*[@id="acc_form"]/table/tbody/tr[2]/' +\
+			'td[2]/table/tbody/tr/td/form/table/tbody/tr[3]/td[2]/table/' +\
+			'tbody/tr/td/img'
+			# loop will break when the more button can't be found
+			while True: 
+				more_btn = browser.find_element_by_xpath(more_button_xpath)
+				login = browser.find_element_by_id("login").text
+				password = browser.find_element_by_id("password").text
+				usernames.append(login)
+				passwords.append(password)
+				sleep(random.uniform(min_wait, max_wait))
+				# click for more accounts
+				more_btn.click()
+				# page render is stupid
+				sleep(1)
+		# no more usernames when the login field can't be found
+		except NoSuchElementException, e: 
+			print "No more logins for", url
 		# return the list of tuples for later parsing
-		fakeaccount_response.close()
-		return zip(usernames, passwords, rates, votes)
-	except urllib2.HTTPError, e:
-		print "Error code: ", e.code
-		# in the odd case of 404, keep going
-		if e.code == 404:
-			print url, "- HTTP 404"
-			return None
-		else:
-			raise e(e.fp.read())
-
+		return zip(usernames, passwords)
+	except WebDriverException, e2:
+		print "Oops, gonna have to check the Selenium server"
 
 """
 Write out the results to a file
@@ -101,10 +94,9 @@ def report_results(url, result, result_num, writeout, log):
 	if writeout:
 		write_result(url, result, log)
 
-def main(scrape_file, min_wait=1.0, max_wait=3.5, **kwargs):
+def main(scrape_file, min_wait=4.5, max_wait=5.5, **kwargs):
 	# seed for waiting
 	random.seed()
-
 	try:
 		with open(scrape_file, 'r') as to_scrape:
 			site_counter = kwargs['site_counter'] # loop break, default=1
@@ -114,8 +106,8 @@ def main(scrape_file, min_wait=1.0, max_wait=3.5, **kwargs):
 				url_num = site.rsplit(',')[0].strip()
 				# --skip option takes effect here
 				if int(url_num) == int(site_counter):
-					# get the result, None = failure
-					site_result = scrape(url)
+					# get the result
+					site_result = scrape(url, min_wait, max_wait)
 					if site_result:
 						# record the results
 						report_results(url, site_result,
@@ -125,7 +117,7 @@ def main(scrape_file, min_wait=1.0, max_wait=3.5, **kwargs):
 					if site_counter >= int(kwargs['site_counter']) +\
 					 int(kwargs['max_sites'] - 1):
 						break
-					# don't want to DoS...
+					# don't want to DoS...or get caught
 					sleep(random.uniform(min_wait, max_wait))
 					site_counter += 1
 			
